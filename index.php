0da952441935bc66b7d877658a864ef7704ff8bf
Upek Hansaja <upekhansajabk@gmail.com>
2024-01-08 15:05:47 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fusion Art Gallery</title>
+</head>
+<body>
+    Welcome to Fusion Art Gallery by Fusion Labs
+</body>
+</html>
